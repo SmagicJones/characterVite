d82557481bedd8161ede9ef237ad2b9a4281209e
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-08-12 20:11:36 +0100

    starting the form console logging the array of character

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 import { makePerson, makeSuperPerson } from "./utils/actions";
 
@@ -64,6 +65,22 @@ superPeople.push(megaClaire);
 superPeople.push(megaJack);
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [level, setLevel] = useState("");
+  const array: string[] = [];
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    array.push(name, age, level);
+    const strName = array[0];
+    const ageNum = Number(array[1]);
+    const levelNum = Number(array[2]);
+    const newPerson = makePerson(strName, ageNum, levelNum);
+    people.push(newPerson);
+    console.log(people);
+  }
+
   return (
     <>
       <div className="full-size">
@@ -78,6 +95,74 @@ function App() {
             </div>
           </div>
         </header>
+
+        <section className="m-2">
+          <div className="grid grid-cols-1">
+            <div className="bg-slate-900 rounded-xl text-white p-4">
+              <div className="text-center">
+                <h3>Test Area</h3>
+              </div>
+              <div className="grid md:grid-cols-2 gap-4 m-2">
+                <div className="bg-slate-500 rounded-xl p-4">
+                  <form
+                    className="grid grid-cols-2 gap-4 text-black"
+                    onSubmit={handleSubmit}
+                  >
+                    <div className="flex justify-center items-center">
+                      <label htmlFor="name" className="text-2xl">
+                        Name
+                      </label>
+                    </div>
+                    <input
+                      className="p-4 rounded-xl"
+                      name="name"
+                      id="name"
+                      type="text"
+                      onChange={(e) => {
+                        setName(e.target.value);
+                      }}
+                    />
+                    <div className="flex justify-center items-center">
+                      <label className="text-2xl" htmlFor="age">
+                        Age
+                      </label>
+                    </div>
+
+                    <input
+                      className="p-4 rounded-xl"
+                      name="age"
+                      id="age"
+                      type="text"
+                      onChange={(e) => {
+                        setAge(e.target.value);
+                      }}
+                    />
+                    <div className="flex justify-center items-center">
+                      <label className="text-2xl" htmlFor="level">
+                        Level
+                      </label>
+                    </div>
+
+                    <input
+                      className="p-4 rounded-xl"
+                      name="level"
+                      id="level"
+                      type="text"
+                      onChange={(e) => {
+                        setLevel(e.target.value);
+                      }}
+                    />
+                    <div></div>
+                    <button className="p-4 bg-slate-900 rounded-xl text-white">
+                      Submit
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
         <main>
           <div className="grid grid-cols-1 m-2">
             <div className="bg-slate-500 rounded-xl text-white p-4">
